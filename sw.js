571ba9cf6fb92588cc6cc6c09b560934a2a2fb23
itnesh <160150146+itnesh@users.noreply.github.com>
2026-07-28 09:46:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hofwerk-shell-v5";
+const CACHE = "hofwerk-shell-v6";
 const SHELL = ["./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./logo.png", "./header-bg.jpg"];
 
 self.addEventListener("install", (e) => {
